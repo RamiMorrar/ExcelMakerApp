ec5a35377f65d233c933df044cfb71e298a7dc8f
exewildwolf@gmail.com <exewildwolf@gmail.com>
2021-09-06 22:17:47 -0700

    1st part

--- ExcelMaker/Program.cs
+++ ExcelMaker/Program.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ExcelMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
@@ -15,9 +16,26 @@ namespace ExcelMaker
             var file = new FileInfo(fileName:@"Demonstration.xlsx");
 
             var people = GetSetupData();
+
+            await SaveExcelFile(people, file);
         }
 
-        static List<Person> GetSetupData()
+        private static Task SaveExcelFile(List<Person> people, FileInfo file)
+        {
+            DeleteIfExists(file);
+
+
+        }
+
+        private static void DeleteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List<Person> GetSetupData()
         {
             List<Person> output = new() // << new C# 9 syntax that means new Person();
             {
