b04642ef2f65464abaa0db0fef57bbaf9d55b10b
exewildwolf@gmail.com <exewildwolf@gmail.com>
2021-09-06 22:14:00 -0700

    Add project files.

---
+++ ExcelMaker/Program.cs
@@ -0,0 +1,32 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExcelMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var file = new FileInfo(fileName:@"Demonstration.xlsx");
+
+            var people = GetSetupData();
+        }
+
+        static List<Person> GetSetupData()
+        {
+            List<Person> output = new() // << new C# 9 syntax that means new Person();
+            {
+                new() {Id = 1 , FirstName = "Rami", LastName = "Morrar"},
+                new() { Id = 1, FirstName = "Terry", LastName = "Bogard" },
+                new() { Id = 1, FirstName = "GoldLewis", LastName = "Dickinson" },
+
+            };
+            return output;
+        }
+    }
+}
